791e6e4cec538362509086d9d240c5c4b610f534
Jaipreeth-J <jaipreethj@gmail.com>
2026-09-01 13:13:47 +0530

    Day 6: balance calculation per group member

--- expense-splitter/backend/src/routes/groups.js
+++ expense-splitter/backend/src/routes/groups.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
 import { createGroup, listGroups, addMember } from '../controllers/groupsController.js';
 import { createExpense, listExpenses } from '../controllers/expensesController.js';
+import { getBalances } from '../controllers/balancesController.js';
 
 const router = Router();
 
@@ -13,5 +14,7 @@ router.post('/:id/members', addMember);
 
 router.post('/:id/expenses', createExpense);
 router.get('/:id/expenses', listExpenses);
+
+router.get('/:id/balances', getBalances);
 
 export default router;
